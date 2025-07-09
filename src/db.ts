ae9e8fdf951f7b336aefb05f5d2bd2eac64eec57
Harsh-singh-9326 <harshS9326@gmail.com>
2025-07-09 22:54:49 +0530

    first commit

---
+++ src/db.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import { Client} from "pg"
+dotenv.config()
+
+const client = new Client({
+    connectionString:process.env.DATABASE_URL,
+    ssl: {
+    rejectUnauthorized: false, // Required for Neon.tech
+  },
+})
+
+client.connect()
+.then(()=>{
+    console.log("you succesfully coonected with database")
+})
+.catch(err=>{
+console.log("you failed to connect with data ",err)
+})
+console.log("ENV:", process.env.DATABASE_URL);
